7c10152ef5afec81cb39d8c5c239a5591b61f87d
user <daradosu@gmail.com>
2015-10-17 19:28:49 +0100

    Add put patch and update methods to Emoji class

--- src/origins/Emoji.php
+++ src/origins/Emoji.php
@@ -5,6 +5,27 @@ namespace Dara\Origins;
 class Emoji extends Model
 {
   
+  public function put($id, $params)
+  {
+    return $this->update($id, $params);
+  }
 
+  public function patch($id, $params)
+  {
+    return $this->update($id, $param);
+  }
+
+  public function update($id, $inputParams)
+  {
+    $emoji = Emoji::find($id);
+
+    $params = $inputParams;
+
+    foreach ($params as $key => $value) {
+        $emoji->$key = $value;
+    }
+
+    return $emoji->save() ? ["msg" => "update successful"] : ["msg" => "Nothing to update"];
+  }
 
 }
