4ad9ccab121511405545abc1491337306a3542d2
user <daradosu@gmail.com>
2015-10-17 20:19:49 +0100

    Update Emoji and User classes with proper comments

--- src/origins/Emoji.php
+++ src/origins/Emoji.php
@@ -5,16 +5,39 @@ namespace Dara\Origins;
 class Emoji extends Model
 {
   
+  /**
+   * Execute the api put call
+   * 
+   * @param  int $id     
+   * @param  array $params 
+   * @return null         
+   */
   public function put($id, $params)
   {
     return $this->update($id, $params);
   }
 
+
+  /**
+   * Execute the api patch call
+   * 
+   * @param  int $id     
+   * @param  array $params 
+   * @return null         
+   */
   public function patch($id, $params)
   {
     return $this->update($id, $param);
   }
 
+
+  /**
+   * Carry out an update using provided parameters
+   * 
+   * @param  int $id          
+   * @param  array $inputParams 
+   * @return array              
+   */
   public function update($id, $inputParams)
   {
     $emoji = Emoji::find($id);
--- src/origins/User.php
+++ src/origins/User.php
@@ -9,12 +9,28 @@ date_default_timezone_set('Africa/Lagos');
 
 class User extends Model
 {
-  
+    
+    /**
+     * Confirm that it's the creator of the emoji that wants to edit it
+     * 
+     * @param  string $userName 
+     * @param  int $emojiId  
+     * @return string           
+     */
     public function checkEmojiOwnership($userName, $emojiId)
     {  
         return $this->getUserId($userName) == $this->getEmojiOwner($emojiId) ? true : false;   
     }
 
+
+    /**
+     * Return user details
+     * 
+     * @param  string $table   
+     * @param  string $column  
+     * @param  string $keyword 
+     * @return string          
+     */
     public function where($table, $column, $keyword)
     {
         $selectValue = $this->connection->prepare('select * from '.$table.' where '.$column.' = \''.$keyword.'\'');
@@ -23,24 +39,55 @@ class User extends Model
         return $selectValue->fetch(PDO::FETCH_ASSOC);
     }
 
+
+    /**
+     * Return User ID
+     * 
+     * @param  string $userName 
+     * @return int           
+     */
     public function getUserId($userName)
     {
         $user = $this->where('users', 'username', $userName);
         return $user['id'];
     }
 
+
+    /**
+     * Get emoji owner
+     * 
+     * @param  string $emojiId 
+     * @return string          
+     */
     public function getEmojiOwner($emojiId)
     {
         $emoji = Emoji::find($emojiId);
         return $emoji->resultRows[0]['created_by'];
     }
 
+
+    /**
+     * Choose to Log user in
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return string           
+     */
     public function login($username, $password)
     {
         $login = $this->authenticate($username, $password);
         return $login["msg"] !== true ? ["msg" => $login["msg"]] : $this->allowLogin($username, $password, $login['id']);   
     }
 
+
+    /**
+     * Log user in
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @param  string $userId   
+     * @return string           
+     */
     public function allowLogin($username, $password, $userId)
     {
         $logUserIn = User::find($userId);
@@ -53,21 +100,52 @@ class User extends Model
         return !$logUserIn->save() ? ["msg" => "An error occurred. Please login again"] : $this->loginSuccessMessage($token);
     }
 
+
+    /**
+     * Return login confirmation
+     * 
+     * @param  string $token 
+     * @return array        
+     */
     public function loginSuccessMessage($token)
     {
         return ["msg" => "Login successful. You can now use the API with your token", "token" => $token];
     }
 
+
+    /**
+     * Authenticate user if validation is successful
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return null           
+     */
     public function authenticate($username, $password)
     {
         return $this->validate($username, $password) ? $this->doAuth($username, $password) : $this->invalidFieldsError();
     }
 
+
+    /**
+     * Validate user input
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return bool           
+     */
     public function validate($username, $password)
     {   
         return trim($username) == '' || trim($password) == '' ? false : true;
     }
 
+
+    /**
+     * Authenticate user
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return array           
+     */
     public function doAuth($username, $password)
     {
         $user = $this->where('users', 'username', $username);
@@ -79,31 +157,74 @@ class User extends Model
         return $password == $user['password'] ? ["msg" => true, "id" => $user['id']] : ["msg" => "Wrong password."];
     }
 
+
+    /**
+     * Register user if validation is successful
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return null           
+     */
     public function register($username, $password)
     {
         return $this->validate($username, $password) ? $this->doRegister($username, $password) : $this->invalidFieldsError();
     }
 
+
+    /**
+     * Return invalid Fields Error
+     * 
+     * @return array 
+     */
     public function invalidFieldsError()
     {
         return ["msg" => "You must enter your username and password."];
     }
 
+
+    /**
+     * Register a new user
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return null           
+     */
     public function doRegister($username, $password)
     {
         return !$this->checkDuplicateUser($username) ? $this->createUser($username, $password) : $this->duplicateUserError();
     }
 
+
+    /**
+     * Check for duplicate user 
+     * 
+     * @param  string $username 
+     * @return null           
+     */
     public function checkDuplicateUser($username)
     {
         return $this->where('users', 'username', $username);
     }
 
+
+    /**
+     * Return duplicate User Error message
+     * 
+     * @return array 
+     */
     public function duplicateUserError()
     {
         return ["msg" => "That username is already taken. Please select another."];
     }
 
+
+    /**
+     * Create new user 
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return array           
+     */
     public function createUser($username, $password)
     {   
 
@@ -119,11 +240,26 @@ class User extends Model
         return $this->save() ? $this->createUserSuccessInfo($token) : ["msg" => "An error occured, registration unsuccessful"];
     }
 
+
+    /**
+     * Return registration confirmation
+     * 
+     * @param  string $token 
+     * @return array        
+     */
     public function createUserSuccessInfo($token)
     {
         return  ["msg" => "Registration successful. You can start utilizing this API with your token", "token" => $token];
     }
 
+
+    /**
+     * Log user out
+     * 
+     * @param  string $username 
+     * @param  string $password 
+     * @return array           
+     */
     public function logout($username, $password)
     {
         $user = $this->doAuth($username, $password);
