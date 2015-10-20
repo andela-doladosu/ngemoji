ba120de855a72e45d1699beb9985c3ae59a05f7a
user <daradosu@gmail.com>
2015-10-20 13:06:39 +0100

    Remove redundant loadenv function from Model class

--- src/origins/Model.php
+++ src/origins/Model.php
@@ -9,10 +9,5 @@ use Dotenv;
 
 class Model extends basemodel
 {
-    public function loadEnv()
-    {
-        $dotenv = new Dotenv\Dotenv(__DIR__.'/../..');
-        $dotenv->load();
-    }
-
+  
 }
